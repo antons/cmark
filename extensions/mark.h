446a1d65f6c4070336b30c1e11064cc935aa4bfb
Anton Sotkov <anton@sotkov.com>
2020-04-27 06:06:46 +0300

    Add mark extension
    ==mark==

---
+++ extensions/mark.h
@@ -0,0 +1,9 @@
+#ifndef CMARK_GFM_MARK_H
+#define CMARK_GFM_MARK_H
+
+#include "cmark-gfm-core-extensions.h"
+
+extern cmark_node_type CMARK_NODE_MARK;
+cmark_syntax_extension *create_mark_extension(void);
+
+#endif
